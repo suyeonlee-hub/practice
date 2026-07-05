dfe5f4254cc2d75b96550ed4e88c90425f2ba334
suyeonlee-hub <syjaydenlee@gmail.com>
2026-07-05 21:34:46 +0800

    feat: rev1 add function

---
+++ main.py
@@ -0,0 +1,8 @@
+from typing import Union
+
+def add(x: Union[int, float], y: Union[int, float]) -> Union[int, float]:
+    return x + y
+
+if __name__ == "__main__":
+    print(add(5, 3))        # Output: 8
+    print(add(5.5, 2.3))    # Output: 7
