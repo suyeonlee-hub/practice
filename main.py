dca63160bb41f47ea7ff5b2ef7f600fa04639812
suyeonlee-hub <syjaydenlee@gmail.com>
2026-07-05 21:41:40 +0800

    feat: rev2
    #1

--- main.py
+++ main.py
@@ -6,3 +6,4 @@ def add(x: Union[int, float], y: Union[int, float]) -> Union[int, float]:
 if __name__ == "__main__":
     print(add(5, 3))        # Output: 8
     print(add(5.5, 2.3))    # Output: 7
+    print(add(5, 2.3))      # Output: 7.3
